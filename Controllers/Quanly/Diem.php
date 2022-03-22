047a6d3e3b9d3d7d8a89f80c5c75e36b2e3ca94a
binhnguyen00 <jackjack2000.kahp@gmail.com>
2022-03-22 12:31:40 +0700

    sua them static method thanh non-static

--- Controllers/Quanly/Diem.php
+++ Controllers/Quanly/Diem.php
@@ -17,20 +17,20 @@ switch ($action) {
 		if (isset($_GET['maSV'])) {
 			$maSV = $_GET['maSV'];
 
-			$dHP = TongDiemChitiet::DiemHP($maSV);
+			$dHP = (new TongDiemChitiet)->DiemHP($maSV);
 		}
 		require_once 'View/Diem/list_diem.php';
 		break;
 	case 'Add_Diem_HP':
-		$list_sv = Sinhvien::List();
-		$list_hp = MonHP::List();
+		$list_sv = (new Sinhvien)->List();
+		$list_hp = (new MonHP)->List();
 		if (isset($_POST['themDiem'])) {
 			$maSV = $_POST['sellist1'];
 			$maM = $_POST['sellist2'];
 			$txt_diemGK = $_POST['txt_diemGK'];
 			$txt_diemTHK = $_POST['txt_diemTHK'];
 
-			if (DiemMHP::ADD($maSV,$maM,$txt_diemGK,$txt_diemTHK)) {
+			if ((new DiemMHP)->ADD($maSV,$maM,$txt_diemGK,$txt_diemTHK)) {
 				$thanhcong = "Thêm điểm thành công";
 			}
 			else
@@ -44,7 +44,7 @@ switch ($action) {
 		if (isset($_GET['maMon'])) {
 			$text_masv = $_GET['maSV'];
 			$text_mamon = $_GET['maMon'];
-			$list_diem_lop_sinhvien = DiemMHP::D_M_SV($text_masv,$text_mamon);
+			$list_diem_lop_sinhvien = (new DiemMHP)->D_M_SV($text_masv,$text_mamon);
 
 			// echo "<pre>";
 			// print_r($list_diem_lop_sinhvien);
@@ -53,7 +53,7 @@ switch ($action) {
 				$txt_diemGK = $_POST['txt_diemGK'];
 				$txt_diemTHK = $_POST['txt_diemTHK'];
 
-				if (DiemMHP::Edit($text_masv,$text_mamon,$txt_diemGK,$txt_diemTHK)) {
+				if ((new DiemMHP)->Edit($text_masv,$text_mamon,$txt_diemGK,$txt_diemTHK)) {
 					header('location:index.php?controllers=diem&action=QL_Diem');
 				}
 				else
@@ -72,7 +72,7 @@ switch ($action) {
 			
 			// echo "Mã sinh viên là: ".$text_masv."<br/>";
 			// echo "Mã Môn là: ".$text_mamon."<br/>";
-			if (DiemMHP::Delete($text_masv,$text_mamon)) {
+			if ((new DiemMHP)->Delete($text_masv,$text_mamon)) {
 				header('location:index.php?controllers=diem&action=QL_Diem');
 			}
 			else
@@ -82,37 +82,37 @@ switch ($action) {
 		}
 		break;
 	case 'QL_Diem':
-		$list_lop = Lop::List();
+		$list_lop = (new Lop)->List();
 		if (isset($_GET['maLop'])) {
 			$maLop = $_GET['maLop'];
-			$list_sv = DiemMHP::Lop_Sinhvien($maLop);
+			$list_sv = (new DiemMHP)->Lop_Sinhvien($maLop);
 		}
 		if (isset($_GET['maSV'])) {
 			$text_masv = $_GET['maSV'];
-			$dHP = DiemMHP::List($text_masv);
+			$dHP = (new DiemMHP)->List($text_masv);
 		}
 
 		require_once 'View/Diem/xl_diem.php';
 		break;
 	case 'Tonghopdiem':
-		$list_lop = Lop::List();
+		$list_lop = (new Lop)->List();
 		if (isset($_POST["Hienthi"])) {
 			$txt_malop = $_POST['txt_malop'];
-			$list_lop_sinhvien = Lop::Lop_Sinhvien($txt_malop);
+			$list_lop_sinhvien = (new Lop)->Lop_Sinhvien($txt_malop);
 		}
 		if (isset($_POST["xem"])) {
 			if(isset($_POST['txt_masinhvien'])){
 				$txt_masinhvien = $_POST['txt_masinhvien'];
 
-				$sv = Sinhvien::GetId($txt_masinhvien);
-				$ttDiem = TongDiemChitiet::TDiem($txt_masinhvien);
+				$sv = (new Sinhvien)->GetId($txt_masinhvien);
+				$ttDiem = (new TongDiemChitiet)->TDiem($txt_masinhvien);
 				
 			}
 		}
 		require_once 'View/Tonghopdiemsinhvien.php';
 		break;
 	case 'Thong_ke':
-		$sv = Sinhvien::List();
+		$sv = (new Sinhvien)->List();
 		$dem=0;
 		foreach ($sv as $value) {
 			$ma_sv_l[] = $value['ma_sv'];
@@ -121,14 +121,14 @@ switch ($action) {
 		for($i = 0; $i < $dem; $i++)
 		{  	
 			//echo "Mã sinh viên [$i] là: ".$ma_sv_l[$i]."<br/>";
-			$sv_tc_sv = TongDiemChitiet::TDiem($ma_sv_l[$i]);
+			$sv_tc_sv = (new TongDiemChitiet)->TDiem($ma_sv_l[$i]);
 
 			$TongSTC = 0;
 			$TongHDS = 0;
 			foreach ($sv_tc_sv as $value) {
 				$diemHP = round(($value['diem_giua_ky']*0.3)+($value['diem_thi_hp']*0.7),1);
-				$diemchu = TongDiemChitiet::DC($diemHP);
-				$diemheso = TongDiemChitiet::HDS($diemHP);
+				$diemchu = (new TongDiemChitiet)->DC($diemHP);
+				$diemheso = (new TongDiemChitiet)->HDS($diemHP);
 
 				$TinhDHS = $value['sotinchi']*$diemheso;
 
@@ -136,7 +136,7 @@ switch ($action) {
 				$TongHDS += $TinhDHS;
 			}
 			$tbtk = round($TongHDS/$TongSTC,2);
-            $xltk = TongDiemChitiet::XL_TK($TongHDS/$TongSTC);
+            $xltk = (new TongDiemChitiet)->XL_TK($TongHDS/$TongSTC);
 
             // echo "Tổng_STC: ".$TongSTC."<br/>";
             // echo "TB_TK: ".$tbtk."<br/>";
@@ -161,6 +161,3 @@ switch ($action) {
 		echo "Trang không tồn tại";
 		break;
 }
-
-
- ?>
